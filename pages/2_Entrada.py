8dcdc20f367e44e151fdcf74e29bdde389e607ab
osmarjunioor <juninho_12006@hotmail.com>
2023-12-05 19:32:38 +0000

    111

--- pages/2_Entrada.py
+++ pages/2_Entrada.py
@@ -20,7 +20,7 @@ with st.form('my_form'):
     submitted = st.form_submit_button("Enviar")
 
 if submitted:
-    new_data = {"Nome": name, "Valor": entrada, "Data": data, "Origem": origem}
+    new_data = {"Nome": name, "Valor": int(entrada), "Data": data, "Origem": origem}
     df = df.append(new_data, ignore_index=True)
     df.to_csv('content/gasto.csv', index=False)
 
