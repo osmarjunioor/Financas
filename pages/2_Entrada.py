2429657ef748c7a0a55918c1a20e91738153c48b
osmarjunioor <juninho_12006@hotmail.com>
2023-12-05 19:47:33 +0000

    1111

--- pages/2_Entrada.py
+++ pages/2_Entrada.py
@@ -5,18 +5,19 @@ st.set_page_config(layout="wide")
 
 df = pd.read_csv('content/gasto.csv')
 
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-    name = st.text_input('Qual seu nome?', '')
-with col2:
-    entrada = st.text_input('Adicione o valor de entrada de dinheiro:', '')
-with col3:
-    data = st.text_input('Adicione a data da entrada:', '')
-with col4:
-    origem = st.text_input('De onde veio?', '')
 
 with st.form('my_form'):
+    col1, col2, col3, col4 = st.columns(4)
+    
+    with col1:
+    name = st.text_input('Qual seu nome?', '')
+    with col2:
+    entrada = st.text_input('Adicione o valor de entrada de dinheiro:', '')
+    with col3:
+    data = st.text_input('Adicione a data da entrada:', '')
+    with col4:
+    origem = st.text_input('De onde veio?', '')
+
     submitted = st.form_submit_button("Enviar")
 
 if submitted:
