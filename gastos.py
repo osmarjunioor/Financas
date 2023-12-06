e967b92356e1274914bf6a210baaa0455c92bac9
osmarjunioor <juninho_12006@hotmail.com>
2023-12-06 17:43:34 +0000

    11

--- gastos.py
+++ gastos.py
@@ -26,7 +26,7 @@ PRODUCTS = [
 # Establishing a Google Sheets connection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Fetch existing vendors data
+# Fetch existing vendors data1
 existing_data = conn.read(worksheet="Vendors", usecols=list(range(6)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
